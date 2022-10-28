46de8aea170f6ca9ab86f145839e5f46c3c1e337
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2022-10-27 22:52:17 -0300

    seed base pronta

--- seeders/20221028000420-base_pesquisa.js
+++ seeders/20221028000420-base_pesquisa.js
@@ -3,23 +3,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('pesquisas', [{
+      titulo: 'Biblioteca Digital de Trabalhos de Conclusão de Curso Aprovados',
+      tipo: 'Registro de Software',
+      discenteId: 1,
+      docenteId: 1,
+      data_apresentacao: '12-20-2022',
+      palavras_chave: 'Biblioteca Digital, Ciência Aberta, Acervo IFMS',
+      resumo: 'Este trabalho tem como objetivo construir uma biblioteca digital para que a comunidade acadêmica do IFMS campus Nova Andradina tenha acesso mais rápido, transparente e democrático à produção científica. Ademais, espera-se que o sistema desenvolvido seja mais simples, usável e acessível do que outros sistemas mais complexos utilizados em grandes universidades, por exemplo.',
+      url_download: 'c8519999-40e1-4ef5-9c2d-86981366ff5f',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('pesquisas', null, {})
   }
 };
