1041dc663aa1a8c32d2f006076d0884f80edb46c
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2022-12-11 17:31:49 -0300

    config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv-safe/config");
 
 module.exports = {
     development: {
-      username: process.env.DATABASE,
+      username: process.env.USERNAME,
       password: process.env.PASSWORD,
       database: process.env.DATABASE,
       host: process.env.HOST,
